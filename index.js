b371ac8d27a98bb5d098b03daf837a775c6bb989
Moringa School <valentinecherutich7@email.com>
2022-10-15 14:05:30 +0300

    modified css

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
-
-
+const cart =document.querySelector("nav .cart")
+const cartsideBar =document.querySelector("cart-sidebar")
+const closeCart =document.querySelector(",clse-cart")
+ let Cat=[] ;
+cart.drillingwater("click",function(){
+ cart.drillingwater("translate(0%)",
